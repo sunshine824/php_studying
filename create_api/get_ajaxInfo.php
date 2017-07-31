a26464b2dd4a5de689527aa0d52b386d4bb471ba
sunshine824 <cx790168710@aliyun.com>
2017-07-31 16:09:35 +0800

    前后端数据交互demo

--- create_api/get_ajaxInfo.php
+++ create_api/get_ajaxInfo.php
@@ -26,15 +26,6 @@ mysqli_query($connect, 'set names utf8');
 $data = mysqli_fetch_all(mysqli_query($connect, "select username from userinfo"), MYSQLI_ASSOC);
 $len = count($data);
 
-function checkInUsername($data,$len,$username){
-    for ($i = 0; $i < $len; $i++) {
-        if($username==$data[$i]['username']){
-            return false;
-        }
-    }
-    return true;
-}
-
 if(checkInUsername($data,$len,$username)){
     $result = mysqli_query($connect, "insert into userinfo values (NULL,'$username','$password')");
     if($result){
@@ -46,6 +37,28 @@ if(checkInUsername($data,$len,$username)){
     postInfo(null,'用户已存在',0);
 }
 
+/**
+ * 判断用户名是否存在
+ * @param Array $data          数据库查询数据
+ * @param int $len             数据长度
+ * @param string $username     用户名
+ * @return bool
+ */
+function checkInUsername($data,$len,$username){
+    for ($i = 0; $i < $len; $i++) {
+        if($username==$data[$i]['username']){
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * 数据返回函数
+ * @param Array $data     //返回数据
+ * @param string $info    //返回信息
+ * @param int $code       //返回状态码
+ */
 function postInfo($data,$info,$code){
     $output = [
         'data' => $data,
