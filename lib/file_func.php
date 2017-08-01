e9d59b75f6583ac7d2118234e8b79c028ad81d6a
sunshine824 <cx790168710@aliyun.com>
2017-08-01 15:51:48 +0800

    常用文件函数封装

--- lib/file_func.php
+++ lib/file_func.php
@@ -431,6 +431,87 @@ function upload_file($fileInfo,$uploadPath='./uploads',$imageFlag=true,$allowExt
     }
 }
 
+/**
+ * 压缩单个文件
+ * @param string $filename 文件名
+ * @return bool
+ */
+function zip_file($filename,$deleteOldFile=true){
+    if(!is_file($filename)){
+        return false;
+    }
+    $zip=new ZipArchive();
+    $zipName=basename($filename).'.zip';
+    //打开指定压缩包，不存在则创建，存在则覆盖
+    if($zip->open($zipName,ZipArchive::CREATE|ZipArchive::OVERWRITE)){
+        //将文件添加到压缩包中
+        if($zip->addFile($filename)){
+            if($deleteOldFile){
+                @unlink($filename);
+            }
+        }
+        $zip->close();
+        return true;
+    }else{
+        return false;
+    }
+}
+//var_dump(zip_file('./1.txt',true));
+
+/**
+ * 多文件压缩
+ * @param string $zipName    压缩包名称
+ * @param array ...$files    需要压缩文件名，可以是多个
+ * @return bool
+ */
+function zip_files($zipName,...$files){
+    //检测压缩包名称是否正确
+    $zipExt=strtolower(pathinfo($zipName,PATHINFO_EXTENSION));
+    if('zip'!==$zipExt){
+        return false;
+    }
+    $zip=new ZipArchive();
+    if($zip->open($zipName,ZipArchive::CREATE|ZipArchive::OVERWRITE)){
+        foreach ($files as $file){
+            if(is_file($file)){
+                $zip->addFile($file);
+            }
+        }
+        $zip->close();
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//var_dump(zip_files('test1.zip','upload/doUpload.php','upload/upload.html'));
+
+
+/**
+ * 解压缩
+ * @param string $zipName  压缩包名称
+ * @param $dest            压缩到指定目录
+ * @return bool
+ */
+function unzip_file($zipName,$dest){
+    //检测要解压的压缩包是否存在
+    if(!is_file($zipName)){
+        return false;
+    }
+    //检测目标路径是否存在
+    if(!is_dir($dest)){
+        mkdir($dest,0777,true);
+    }
+    $zip=new ZipArchive();
+    if($zip->open($zipName)){
+        $zip->extractTo($dest);
+        $zip->close();
+        return true;
+    }else{
+        return false;
+    }
+}
+//var_dump(unzip_file('test1.zip','a'));
 
 
 
